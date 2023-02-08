c14ad1c0e7a81436a4b02d964eb0f3e98b0fac7e
Jan Dolanský <dolanovsky@gmail.com>
2023-02-08 18:31:10 +0200

    Fix user breakdown dates

--- frontend/src/components/statistics/UserBreakdown.vue
+++ frontend/src/components/statistics/UserBreakdown.vue
@@ -37,21 +37,23 @@ const quoting = computed(() => objectToArray(authored.value.reduce((group, quote
 
   return group
 }, {} as Record<string, number>)).sort((a, b) => getVal(a) < getVal(b) ? 1 : -1))
+
+// First time quoted, filtering out 1970 dates
+const firstQuoted = computed(() => quoted.value.filter(q => q.createdAt !== 0).at(-1))
+const firstPosted = computed(() => authored.value.filter(a => a.createdAt !== 0).at(-1))
 </script>
 
 <template>
   <div class="quote-container">
-    the dates are wrong i think
-
     <InputSelect v-model:selected="activeUser" icon="e7fd" :options="user.users.map(u => u.username)" />
     <div class="stats-grid user">
       <StatCell str label="Quoted" :data="`${toNum(quoted.length)} - ${percent(quoted.length, quote.quotes.length).toFixed(2)}%`" />
       <StatCell str label="Most quoted by" :data="`${getKey(quotedBy[0])} - ${getVal(quotedBy[0])}`" />
-      <StatCell str label="First time quoted" :data="date.timeShort(quoted[0].createdAt)" />
+      <StatCell str label="First time quoted" :data="firstQuoted ? date.timeShort(firstQuoted.createdAt) : 'Never'" />
 
       <StatCell str label="Posted" :data="`${toNum(authored.length)} - ${percent(authored.length, quote.quotes.length).toFixed(2)}%`" />
       <StatCell str label="Most quoting" :data="`${getKey(quoting[0])} - ${getVal(quoting[0])}`" />
-      <StatCell str label="First time posted" :data="date.timeShort(authored[0].createdAt)" />
+      <StatCell str label="First time posted" :data="firstPosted ? date.timeShort(firstPosted.createdAt) : 'Never'" />
     </div>
   </div>
 </template>
